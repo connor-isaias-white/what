35d93a5f72413d663edb9323ad8c10d93e6cc4e6
connor <connor.white.spam@gmail.com>
2018-10-18 23:06:56 +1100

    Shortend the word list, amd the button called submit and less margin, finished off the javascript, removed the python script and got rid of placeholder text

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-function words() {
-    var x = document.getElementById('letters').value;
-    document.getElementById("answer").innerHTML = x + x;
+function words(file) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText.split("\n");
+                var x = document.getElementById('letters').value;
+                x = x.replace(" ", '').replace(',', '')
+                var answers = [];
+                var text = ''
+                for(i in allText){
+                    if(allText[i].length >= 3 && x.includes(allText[i]) && allText[i] != x){
+                        answers.push(allText[i]);
+                    }
+                }
+                var y;
+                for(y = 0; y < answers.length; y++){
+                    text += "You can't spell "+x+" without " + answers[y] + "<br>";
+                }
+                document.getElementById("answer").innerHTML = text;
+            }
+        }
+    }
+    rawFile.send(null);
 }
